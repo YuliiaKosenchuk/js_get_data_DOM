a8de462549fadc4b93beef279962ff884d158fda
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-07-30 13:59:41 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const populationElements = [
+    ...document.querySelectorAll(".population")
+];
+const convertPopulation = (populationStr)=>{
+    return parseInt(populationStr.replace(/,/g, ""), 10);
+};
+const populations = populationElements.map((item)=>{
+    return convertPopulation(item.textContent);
+});
+const total = populations.reduce((acc, item)=>acc + item, 0);
+const average = total / populations.length;
+const totalPopulation = document.querySelector(".total-population");
+totalPopulation.textContent = total.toLocaleString();
+const averagePopulation = document.querySelector(".average-population");
+averagePopulation.textContent = average.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
